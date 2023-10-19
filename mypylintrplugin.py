6236f3ef86baac88ffc69e49e01e55219a536047
sp suryawanshi <59173100+MrSpSuryawanshi@users.noreply.github.com>
2023-10-20 03:52:41 +0530

    Update mypylintrplugin.py - Adding CUSTOM CODE CHECKERS

--- mypylintrplugin.py
+++ mypylintrplugin.py
@@ -104,6 +104,70 @@ class CustomExecChecker(BaseChecker):
             self.add_message("exec-function", node=node)
 
 
+class CustomOpenChecker(BaseChecker):
+    __implements__ = (IAstroidChecker,)
+
+    name = "custom-open-checker"
+    msgs = {
+        "W9906": (
+            "Avoid using the 'open' function. It can be a security risk.",
+            "custom-open-function",
+            "This message is shown when the 'open' function is used.",
+        ),
+    }
+
+    @check_messages("custom-open-function")
+    def visit_call(self, node):
+        if node.func.as_string() == "open":
+            self.add_message("custom-open-function", node=node)
+
+
+class CustomDropnaChecker(BaseChecker):
+    __implements__ = (IAstroidChecker,)
+
+    name = "custom-dropna-checker"
+
+    def visit_call(self, node):
+        func_name = node.func.as_string()
+        if func_name == "DROPNA":
+            self.add_message("custom-dropna", node=node, args="Avoid using 'DROPNA'. Use more appropriate function names.")
+
+class CustomCodeChecker(BaseChecker):
+    __implements__ = (IAstroidChecker,)
+
+    name = "custom-code-checker"
+
+    def visit_call(self, node):
+        func_name = node.func.as_string()
+        custom_messages = {
+            "ARANGE": "Avoid using 'ARANGE'. Use 'range' or 'numpy.arange' instead.",
+            "DROPNA": "Consider handling missing data more explicitly. Avoid using 'DROPNA'.",
+            "ZEROS_ONES": "Avoid using 'ZEROS_ONES'. Use 'numpy.zeros' or 'numpy.ones' instead.",
+            "MERGE": "Avoid using 'MERGE'. Use 'pandas.merge' instead.",
+            "HEAD_TAIL": "Avoid using 'HEAD_TAIL'. Use more descriptive names.",
+            "APPLY": "Avoid using 'APPLY'. Use more descriptive function names."
+        }
+
+        if func_name in custom_messages:
+            self.add_message("custom-code", node=node, args=custom_messages[func_name])
+
+    def visit_tryfinally(self, node):
+        self.add_message("custom-finally", node=node, args="Avoid using 'finally' blocks. Consider alternative solutions.")
+
+    def visit_import(self, node):
+        imported_module = node.names[0].name
+        if imported_module == "requests":
+            self.add_message("custom-import-requests", node=node, args="Be cautious when using 'requests' for making HTTP requests. Use it responsibly.")
+    
+    def visit_assign(self, node):
+        if isinstance(node.value, ast.Str):
+            variable_name = node.target.name
+            value = node.value.s
+            if "MAGIC_NUMBER" in value:
+                self.add_message("custom-magic-number", node=node, args="Avoid using 'MAGIC_NUMBER'. Use named constants instead.")
+
+
+
 
 def register(linter):
     linter.register_checker(CustomFlaskDebugChecker(linter))
@@ -111,3 +175,6 @@ def register(linter):
     linter.register_checker(CustomWeakHashChecker(linter))
     linter.register_checker(CustomCredentialsChecker(linter))
     linter.register_checker(CustomExecChecker(linter))
+    linter.register_checker(CustomOpenChecker(linter))
+    linter.register_checker(CustomDropnaChecker(linter))
+    linter.register_checker(CustomCodeChecker(linter))
