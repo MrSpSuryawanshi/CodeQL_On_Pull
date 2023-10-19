7d614b2ba0b5e9f77309587e49d7e202179e8bbf
sp suryawanshi <59173100+MrSpSuryawanshi@users.noreply.github.com>
2023-10-20 03:13:02 +0530

    Update final.py to run

--- final.py
+++ final.py
@@ -2,3 +2,4 @@ from pylint.lint import Run
 files_to_lint = ['app3.py']  # Replace with the path to your Python file
 # results = Run(["--rcfile=.pylintrc"] + files_to_lint, exit=False)
 results = Run(["--rcfile=.pylintrc"] + files_to_lint, exit=False)
+
