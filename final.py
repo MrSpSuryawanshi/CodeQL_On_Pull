4fb37f0ab4c66d222019300bc7e7dab42fa2c931
sp suryawanshi <59173100+MrSpSuryawanshi@users.noreply.github.com>
2023-11-08 12:56:09 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,4 +1,4 @@
 from pylint.lint import Run
-files_to_lint = ['app3.py']  # Replace with the path to your Python file
+files_to_lint = ['app3.py','data_leakage.py']  # Replace with the path to your Python file
 # results = Run(["--rcfile=.pylintrc"] + files_to_lint, exit=False)
 results = Run(["--rcfile=.pylintrc"] + files_to_lint, exit=False)
